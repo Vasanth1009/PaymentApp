29dbde6bd73b965a7898a1b9bf253f4e2190f004
Vasanth <vasanthprabhakaran1999@gmail.com>
2021-02-16 22:21:53 +0530

    Removed Unnecessary Usings

--- PaymentAPI/Models/PaymentDetails.cs
+++ PaymentAPI/Models/PaymentDetails.cs
@@ -1,10 +1,5 @@
-﻿using Castle.MicroKernel.SubSystems.Conversion;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PaymentAPI.Models
 {
